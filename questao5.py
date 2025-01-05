5ebcf722e52e1fe530ae451b97f373c86768216f
nathil <nathiellycastro282@gmail.com>
2025-01-04 21:36:01 -0300

    editando

--- questao5.py
+++ questao5.py
@@ -4,7 +4,6 @@
 
 print('primeiro modo\n')
 lista = [3,6,7,8,5,4,2,3,6,4]
-# resultado: [3,6,7,8,5,4,2]
 
 repetidos = []
 nao_repetidos = []
@@ -19,7 +18,7 @@ print('Lista sem valores repetidos:', nao_repetidos)
 
 #2o modo
 
-print('\nSegundo modo\n')
+print('\nsegundo modo\n')
 
 nao_repetidos = list(set(lista))
 print('Lista de valores não repetidos:', nao_repetidos)
